6ffe9a424782ef3dbfdf4b85b21467d14f4e18db
michaelpelagio9830 <michaelpelagio9830@gmail.com>
2020-05-13 23:57:13 +0800

    Updates

---
+++ precache-manifest.5d715b43d7acb2c08f96972fb89ff78b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c57e0f9705e28ca9138b927e2793a13",
+    "url": "/ReactExamination/index.html"
+  },
+  {
+    "revision": "c2f0d7dfc6550b878420",
+    "url": "/ReactExamination/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "75f5f4edeb9cc15a3edf",
+    "url": "/ReactExamination/static/css/3.f77d4db4.chunk.css"
+  },
+  {
+    "revision": "9d84e28bd0afa6b31811",
+    "url": "/ReactExamination/static/css/main.af120dd2.chunk.css"
+  },
+  {
+    "revision": "c2f0d7dfc6550b878420",
+    "url": "/ReactExamination/static/js/2.c7e9ae8e.chunk.js"
+  },
+  {
+    "revision": "1dbbcd7509c864294efebd87723f8709",
+    "url": "/ReactExamination/static/js/2.c7e9ae8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75f5f4edeb9cc15a3edf",
+    "url": "/ReactExamination/static/js/3.e21f1552.chunk.js"
+  },
+  {
+    "revision": "9d84e28bd0afa6b31811",
+    "url": "/ReactExamination/static/js/main.f8625d92.chunk.js"
+  },
+  {
+    "revision": "115b8d11a158b1eb4208",
+    "url": "/ReactExamination/static/js/runtime-main.73f164f0.js"
+  },
+  {
+    "revision": "0814c0221d1354eb117fb266e1247856",
+    "url": "/ReactExamination/static/media/404.0814c022.jpg"
+  }
+]);
